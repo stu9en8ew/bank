ef55a64647f958628976df7353e614f23aea59f5
Jacomo Marcio (Consulente) <marcio.jacomo@consulenti.fabrick.com>
2021-06-09 18:10:02 +0200

    Project Bank

--- src/main/java/it/banking/repository/AccountRepository.java
+++ src/main/java/it/banking/repository/AccountRepository.java
@@ -5,12 +5,10 @@ import it.banking.rto.MoneyTransferRto;
 import it.banking.rto.TransactionRto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @Repository
@@ -19,13 +17,13 @@ public interface AccountRepository {
     @GetMapping("https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts")
     ResponseEntity<BalanceRto> getBalance(@RequestParam @NotNull Long accountId);
 
-    @GetMapping("/api/gbs/banking/v4.0/accounts")
-    ResponseEntity<List<TransactionRto>> getTransactions(@RequestParam @NotNull Long accountId,
+    @GetMapping("https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/{accountId}/transactions")
+    ResponseEntity<List<TransactionRto>> getTransactions(@PathVariable Long accountId,
                                                          @RequestParam String fromAccountingDate,
                                                          @RequestParam String toAccountingDate);
 
-    @PostMapping("/api/gbs/banking/v4.0/accounts/{accountId}/payments/money-transfers")
-    ResponseEntity<MoneyTransferRto> createMoneyTransfer(@RequestBody @NotNull Long accountId,
+    @PostMapping("https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/{accountId}/payments/money-transfers")
+    ResponseEntity<MoneyTransferRto> createMoneyTransfer(@PathVariable Long accountId,
                                                          @RequestBody String receiverName,
                                                          @RequestBody String description,
                                                          @RequestBody String currency,
